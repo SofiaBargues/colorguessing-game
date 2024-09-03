857bd8367ec933b58dd1137a312b5e6f5027b503
Sofia Bargues <barguessofia@gmail.com>
2024-09-03 13:25:28 +0100

    colour

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
 import { useState } from "react";
 import "./App.css";
 
+function elementRandom(elementos: string[]) {
+  const indiceAleatorioElemento = Math.floor(Math.random() * elementos.length);
+  return elementos[indiceAleatorioElemento];
+}
+
+function colorRandom() {
+  const colorsHex = [
+    "#FF0000",
+    "#00FF00",
+    "#0000FF",
+    "#FFFF00",
+    "#FFA500",
+    "#800080",
+  ];
+  return elementRandom(colorsHex);
+}
+
 function App() {
-  const [button1, setButton1] = useState("black");
-  const [button2, setButton2] = useState("violet-900");
-  const [button3, setButton3] = useState("orange-400");
+  const [button1, setButton1] = useState(colorRandom());
+  const [button2, setButton2] = useState(colorRandom());
+  const [button3, setButton3] = useState(colorRandom());
   const [isCorrect, setIsCorrect] = useState("");
 
-  const mainColor = "black";
+  const arrButtons = [button1, button2, button3];
+
+  const [winnerColor, setWinnerColor] = useState(elementRandom(arrButtons));
+
+  const mainColor = winnerColor;
 
   return (
     <div className="flex flex-col items-center">
-      <div className={"h-[100px] w-[100px] bg-black [" + mainColor + "]"}></div>
+      <div
+        style={{
+          backgroundColor: winnerColor,
+        }}
+        className={"h-[100px] w-[100px] " + mainColor}
+      ></div>
       <div>
         <button
           onClick={() => {
@@ -20,7 +46,6 @@ function App() {
             } else {
               setIsCorrect("false");
             }
-            console.log(isCorrect);
           }}
           className="bg-slate-300 m-3"
         >
